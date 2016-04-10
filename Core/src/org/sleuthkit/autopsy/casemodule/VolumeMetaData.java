ac1cd1a03c47041899e06e2e0eea4fe2c7c3e9b4
Mathias Vetsch <m.vetsch@gmail.com>
2016-04-10 19:55:31 +0200

    added static method for gathering volumeMetaData object

--- Core/src/org/sleuthkit/autopsy/casemodule/VolumeMetaData.java
+++ Core/src/org/sleuthkit/autopsy/casemodule/VolumeMetaData.java
@@ -5,46 +5,84 @@
  */
 package org.sleuthkit.autopsy.casemodule;
 
+import java.util.LinkedList;
+import java.util.List;
+import org.openide.util.Exceptions;
+import org.sleuthkit.datamodel.SleuthkitJNI;
+import org.sleuthkit.datamodel.TskCoreException;
+
 /**
  *
  * @author root
  */
 class VolumeMetaData {
 
-	private String path;
-	private long partitionNumber;
-	private long offSet;
-	private byte[] firstSector;
+    private String path;
+    private long partitionNumber;
+    private long offSet;
+    private byte[] firstSector;
 
-	public long getOffSet() {
-		return offSet;
-	}
+    public long getOffSet() {
+        return offSet;
+    }
 
-	public long getPartitionNumber() {
-		return partitionNumber;
-	}
+    public long getPartitionNumber() {
+        return partitionNumber;
+    }
 
-	public byte[] getFirstSector() {
-		return firstSector;
-	}
+    public byte[] getFirstSector() {
+        return firstSector;
+    }
 
-	public String getPath() {
-		return path;
-	}
+    public String getPath() {
+        return path;
+    }
 
-	public void setOffSet(long offset) {
-		this.offSet = offset;
-	}
+    public void setOffSet(long offset) {
+        this.offSet = offset;
+    }
 
-	public void setPartitionNumber(long pn) {
-		this.partitionNumber = pn;
-	}
+    public void setPartitionNumber(long pn) {
+        this.partitionNumber = pn;
+    }
 
-	public void setFirstSector(byte[] sector) {
-		this.firstSector = sector;
-	}
+    public void setFirstSector(byte[] sector) {
+        this.firstSector = sector;
+    }
 
-	public void setPath(String path) {
-		this.path = path;
-	}
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public static List<VolumeMetaData> getVolumeMetaData(String image) {
+        String[] imageArray = {image};
+
+        List<VolumeMetaData> result = new LinkedList<>();
+
+        try {
+            long handle = SleuthkitJNI.openImage(imageArray);
+            long vsHandle = SleuthkitJNI.openVs(handle, 0);
+
+            for (int volumeIndex = 0; volumeIndex < 5; volumeIndex++) {
+                VolumeMetaData volumeMetaData = new VolumeMetaData();
+                volumeMetaData.setPartitionNumber(volumeIndex);
+
+                volumeMetaData.setOffSet(SleuthkitJNI.getVolOffset(vsHandle, volumeIndex));
+
+                long volumePointer = SleuthkitJNI.openVsPart(vsHandle, volumeIndex);
+
+                byte readBuffer[] = new byte[512];
+                long c = SleuthkitJNI.readVsPart(volumePointer, readBuffer, 0, 512);
+                volumeMetaData.setFirstSector(readBuffer);
+
+                volumeMetaData.setPath(image);
+
+                result.add(volumeMetaData);
+            }
+
+        } catch (TskCoreException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        return result;
+    }
 }
